6c9b98eaa790cc87c9458a6c5b6ebb78dee33a7e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-28 18:39:52 +0700

    fix env mongo uri

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://fariz:fariz@cluster0.ilugg.mongodb.net/mongodb-golang?retryWrites=true&w=majority"))
+	//client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,16 +46,39 @@ func main() {
 
 	fmt.Println(databases)
 
+	// CREATE DATABASE
 	quickstartDatabase := client.Database("quickstart")
+	// CREATE COLLECTION
 	podcastsCollection := quickstartDatabase.Collection("podcasts")
-	//episodesCollection := quickstartDatabase.Collection("episodes")
+	episodesCollection := quickstartDatabase.Collection("episodes")
 
+	// INSERT
 	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
-		{Key: "title", Value: "The Go Programming Language POdcast"},
-		{Key: "author", Value: "Fariz"},
+		{"title", "The Go Programming Language POdcast"},
+		{"author", "Fariz"},
+		{"tags", bson.A{"golang", "programming", "go"}},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(podcastResult.InsertedID)
+	// INSERT MANY
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Episode #1"},
+			{"description", "This is the first episode "},
+			{"duration", 25},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Episode #3"},
+			{"description", "This is the third episode "},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(episodeResult.InsertedIDs)
 }
